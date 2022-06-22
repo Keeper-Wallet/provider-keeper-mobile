2bab1c243ea49eaf56100f30a97f6876240370a6
smelnikov <smelnikov@web3tech.ru>
2022-06-22 10:25:20 +0400

    eslint webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+/* eslint @typescript-eslint/no-var-requires: "off" */
 const { ProvidePlugin } = require('webpack');
 const DotenvPlugin = require('dotenv-webpack');
 const rimraf = require('rimraf');
